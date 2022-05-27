3a36dde0a2571cd3bf078188b9d2ccb84e1563b9
ptuzinek <przeo1996@gmail.com>
2022-05-27 16:33:08 +0200

    init commit

---
+++ bin/squash_test.dart
@@ -0,0 +1,5 @@
+import 'package:squash_test/squash_test.dart' as squash_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${squash_test.calculate()}!');
+}
---
+++ test/squash_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:squash_test/squash_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
